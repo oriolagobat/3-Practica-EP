602c8bbc51a1abccdd3f929c8fc8ac611fb01b2b
Oriol Agost Batalla <oriolagobat@gmail.com>
2021-12-28 09:57:07 +0100

    Doing Unified Platform

--- src/UnifiedPlatform.java
+++ src/UnifiedPlatform.java
@@ -3,23 +3,29 @@ import data.Nif;
 import data.PINcode;
 import data.Password;
 import exceptions.*;
+import services.CertificationAuthority;
 
 import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Hashtable;
 
 public class UnifiedPlatform {
+    Nif citizenNif;
+    DocPath savePath;
+
     HashMap<String, String> aapp;
     HashMap<String, ArrayList<String>> services;
-    String chosenService = null;
+    ArrayList<String> possibleAuthMethods;
+    CertificationAuthority authMethod;
 
     public UnifiedPlatform() {
         this.aapp = new HashMap<>();
         setAapp();
         this.services = new HashMap<>();
         setServices();
+        this.possibleAuthMethods = new ArrayList<>();
+        setAuthMethods();
     }
 
     private void setAapp() {
@@ -70,14 +76,23 @@ public class UnifiedPlatform {
         services.put(key, values);
     }
 
+    private void setAuthMethods() {
+        possibleAuthMethods.add("Cl@ve PIN");
+        possibleAuthMethods.add("Cl@ve Permanente");
+        possibleAuthMethods.add("Certificado digital");
+
+        // WE COULD ADD MORE METHODS //
+    }
+
     public void processSearcher() {
+        System.out.println("És procedeix a usar el buscardor de tràmits");
+        System.out.println("És desplega el buscador");
     }
 
     public void enterKeyWords(String keyWord) throws AnyKeyWordProcedureException {
         String result = aapp.get(keyWord);
         if (result == null) throw new AnyKeyWordProcedureException("El tràmit buscat no és troba...");
         else {
-            chosenService = result;
             switch (result) {
                 case "SS":
                     selectSS();
@@ -98,46 +113,58 @@ public class UnifiedPlatform {
     }
 
     public void selectSS() {
-        selectCitizens();
+        System.out.println("Se hace click en la sección SS del mosaico inicial");
     }
 
     public void selectCitizens() {
-        selectReports();
+        System.out.println("Se hace click en el enlace Ciudadanos de la sección de la SS");
     }
 
     public void selectReports() {
-
+        System.out.println("Se hace click en el enlace Informes y certificados del apartado Ciudadanos de la SS");
     }
 
     public void selectCertificationReport(byte opc) {
-
+        // 0 is Laboral Life Doc
+        // 1 SS affiliation number
+        // We do this to acces through the dictionary
+        // ASSUMING THAT SERCIVES IN THE DICTIONARY WILL BE IN THE SAME ORDER AS IN THE WEB PAGE
+        ArrayList<String> ssServices = services.get("SS");
+        String selectedService = ssServices.get(opc);
+        System.out.println("Se selecciona para obtener el informe " + selectedService);
     }
 
     public void selectAuthMethod(byte opc) {
-
+        String selectedAuthMethod = possibleAuthMethods.get(opc);
+        System.out.println("Se selecciona el método de autenticación " + selectedAuthMethod);
     }
 
     public void enterNIFandPINobt(Nif nif, Date valDate) throws NifNotRegisteredException, IncorrectValDateException, AnyMobileRegisteredException, ConnectException {
-
+        // Assuming auth method is Cl@ve PIN //
+        this.citizenNif = nif;  // We set the class's nif to the one we got through parameter
+        authMethod.sendPIN(nif, valDate);
+        System.out.println("Se envia el PIN al usuario con DNI: " + nif);
+        // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA FALTE VALIDAR LA DATA COM ÉS FA AIXÒ AAAAAAAAAAAAAA
     }
 
     public void enterPIN(PINcode pin) throws NotValidPINException, NotAffiliatedException, ConnectException {
-
+        authMethod.checkPIN(citizenNif, pin);
     }
 
     public void enterCred(Nif nif, Password passwd) throws NifNotRegisteredException, NotValidCredException, AnyMobileRegisteredException, ConnectException {
-
+        // OPCIONAL
     }
 
     private void printDocument() throws BadPathException, PrintingException {
-
+        System.out.println("Se envia el documento para su impresión");
     }
 
     private void downloadDocument() throws BadPathException, PrintingException {
-
+        System.out.println("Se descarga el documento");
     }
 
     private void selectPath(DocPath path) throws BadPathException {
-
+        this.savePath = path;
+        System.out.println("Se ha seleccionado el path: " + path + " para guardar el documento");
     }
 }
